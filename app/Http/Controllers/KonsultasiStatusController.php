320de04bfcecffc0992cea81349263e4765fc986
Satrio Jati Wicaksono <48653478+satriotol@users.noreply.github.com>
2023-01-14 18:36:16 +0700

    konsultasi and user details

---
+++ app/Http/Controllers/KonsultasiStatusController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\KonsultasiStatus;
+use Illuminate\Http\Request;
+
+class KonsultasiStatusController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\KonsultasiStatus  $konsultasiStatus
+     * @return \Illuminate\Http\Response
+     */
+    public function show(KonsultasiStatus $konsultasiStatus)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\KonsultasiStatus  $konsultasiStatus
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(KonsultasiStatus $konsultasiStatus)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\KonsultasiStatus  $konsultasiStatus
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, KonsultasiStatus $konsultasiStatus)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\KonsultasiStatus  $konsultasiStatus
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(KonsultasiStatus $konsultasiStatus)
+    {
+        //
+    }
+}
---
+++ database/migrations/2023_01_14_183306_create_user_details_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_details', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('born_place', 255);
+            $table->date('birth');
+            $table->enum('gender', ['Pria', 'Wanita']);
+            $table->longText('address');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_details');
+    }
+};
