3b53f284bbefbbbb17f45724918839c9b2179fc5
Satrio Jati Wicaksono <48653478+satriotol@users.noreply.github.com>
2023-03-09 12:54:34 +0700

    fixing navbar

--- resources/views/frontend_layouts/navbar.blade.php
+++ resources/views/frontend_layouts/navbar.blade.php
@@ -18,10 +18,6 @@
                         <div class="header-right-img f-right">
                             <img src="{{ asset('frontend_assets/img/shape/line-1.png') }}" alt="">
                         </div>
-                        {{-- <div class="search-icon f-right d-none d-lg-block">
-                                <a href="#" data-toggle="modal" data-target="#search-modal"><i
-                                        class="far fa-search"></i></a>
-                            </div> --}}
                     </div>
                     <div class="main-menu text-right">
                         <nav id="mobile-menu">
@@ -48,52 +44,6 @@
                     <div class="mobile-menu"></div>
                 </div>
             </div>
-        </div>
-    </div>
-    <div class="extra-info">
-        <div class="close-icon">
-            <button>
-                <i class="far fa-window-close"></i>
-            </button>
-        </div>
-        <div class="logo-side mb-30">
-            <a href="#">
-                <img src="{{ asset('frontend_assets/img/logo/white.png') }}" alt="" />
-            </a>
-        </div>
-        <div class="instagram">
-            <a href="#">
-                <img src="{{ asset('frontend_assets/img/portfolio/p1.jpg') }}" alt="">
-            </a>
-            <a href="#">
-                <img src="{{ asset('frontend_assets/img/portfolio/p2.jpg') }}" alt="">
-            </a>
-            <a href="#">
-                <img src="{{ asset('frontend_assets/img/portfolio/p3.jpg') }}" alt="">
-            </a>
-            <a href="#">
-                <img src="{{ asset('frontend_assets/img/portfolio/p4.jpg') }}" alt="">
-            </a>
-            <a href="#">
-                <img src="{{ asset('frontend_assets/img/portfolio/p5.jpg') }}" alt="">
-            </a>
-            <a href="#">
-                <img src="{{ asset('frontend_assets/img/portfolio/p6.jpg') }}" alt="">
-            </a>
-        </div>
-        <div class="social-icon-right mt-20">
-            <a href="#">
-                <i class="fab fa-facebook-f"></i>
-            </a>
-            <a href="#">
-                <i class="fab fa-twitter"></i>
-            </a>
-            <a href="#">
-                <i class="fab fa-google-plus-g"></i>
-            </a>
-            <a href="#">
-                <i class="fab fa-instagram"></i>
-            </a>
         </div>
     </div>
 </header>
