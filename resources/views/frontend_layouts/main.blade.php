d5356338124c59def7923211b838f3e817312ec4
Satrio Jati Wicaksono <48653478+satriotol@users.noreply.github.com>
2023-03-09 11:40:03 +0700

    fixing

--- resources/views/frontend_layouts/main.blade.php
+++ resources/views/frontend_layouts/main.blade.php
@@ -3,6 +3,7 @@
 
 <head>
     <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
     <title>PUSPAGA - Pusat Pembelajaran Keluarga</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="shortcut icon" type="image/x-icon" href="{{ asset('frontend_assets/img/logo/logokota.png') }}">
