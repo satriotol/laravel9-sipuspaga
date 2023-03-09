0e4aee6b0faa60b3f76d0cf4d03625ae39fabb7a
Satrio Jati Wicaksono <48653478+satriotol@users.noreply.github.com>
2023-03-09 11:39:42 +0700

    fixing

--- resources/views/frontend_layouts/main.blade.php
+++ resources/views/frontend_layouts/main.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="utf-8">
     <title>PUSPAGA - Pusat Pembelajaran Keluarga</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="shortcut icon" type="image/x-icon" href="{{ asset('frontend_assets/img/logo/logokota.png') }}">
     <!-- Place favicon.ico in the root directory -->
 
