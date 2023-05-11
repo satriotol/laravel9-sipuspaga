857009501aa585c0df74e821e677acc10daf007a
Satrio Jati Wicaksono <48653478+satriotol@users.noreply.github.com>
2023-05-11 13:20:09 +0700

    fixing

--- app/Http/Controllers/WhatsappController.php
+++ app/Http/Controllers/WhatsappController.php
@@ -14,7 +14,7 @@ class WhatsappController extends Controller
         $message = preg_replace("/(\n)/", "<ENTER>", $message);
         $message = preg_replace("/(\r)/", "<ENTER>", $message);
 
-        $data = array("phone_no" => $phone, "key" => "e4d16a0772635a648acd790503fe71a9ebcd9f538952dfbc", "message" => $message);
+        $data = array("phone_no" => $phone, "key" => "e4d16a0772635a648acd790503fe71a9ebcd9f538952dfbc", "message" => "SIPUSPAGA \n\n" . $message);
         $data_string = json_encode($data);
         $ch = curl_init('http://116.203.92.59/api/send_message');
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
